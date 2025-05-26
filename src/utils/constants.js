5c0da5483b93b7389b64223f8496555211bbfd05
Gia Duc <giaduc19042004@gmail.com>
2025-05-26 12:57:30 +0700

    fix build mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
-export const API_ROOT = 'http://localhost:2004';
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:2004'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://shop-api-w5ss.onrender.com'
+}
+
+export const API_ROOT = apiRoot
