e95f81f5f507261784b9f2ebe944422fa97bab42
Gia Duc <giaduc19042004@gmail.com>
2025-10-30 11:57:04 +0700

    create sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Admin from './pages/Admin/Admin'
 import './App.scss'
 import Menu from './pages/Admin/Menu/Menu'
 import Branch from './pages/Admin/Branch/Branch'
+import SignIn from './pages/Admin/SignIn/SignIn'
+import { ToastContainer, Slide, Zoom, Flip, Bounce } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -13,7 +16,16 @@ function App() {
         <Route path="/admin" element={<Admin />} />
         <Route path="/admin/menu" element={<Menu />} />
         <Route path="/admin/branches" element={<Branch />} />
+        <Route path="/admin/sign-in" element={<SignIn />} />
       </Routes>
+
+      <ToastContainer 
+        position='bottom-right'
+        pauseOnHover
+        toastClassName='custom-toast'
+        theme='colored'
+        transition={Flip}
+      />
     </Router>
   )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,6 @@
 import React, { useEffect } from 'react'
-import { fetchProductDetailsAPI } from '~/apis'
 
 function HomePage() {
-
-  useEffect(() => {
-    console.log("chay")
-    const fetchData = async () => {
-      const data = await fetchProductDetailsAPI()
-      console.log(data)
-    }
-    fetchData()
-  }, [])
 
   return (
     <></>
